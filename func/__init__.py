1e8d67d9b7f35fadf37005b1a7a422b9c7f8bd59
aronabot <gnl5672@gmail.com>
2021-03-18 00:40:53 +0900

    삭제 func의 connect와 updater 패키지를 update로 옮김

--- func/__init__.py
+++ func/__init__.py
@@ -1,5 +1,5 @@
 from .termevent import *
+from .update import *
 from .manager import *
 from .character import *
-from .connect import *
 from .aronaLogger import *
--- func/connect.py
+++
@@ -1,3 +0,0 @@
-class Connect:
-    def __init__(self):
-        pass
